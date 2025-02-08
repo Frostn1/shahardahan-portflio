344d1cb8e9b09f28b7d992d3e38fdcc0a5875798
Sean Dahan <aui.svi@gmail.com>
2025-02-08 21:23:00 +0200

    Fixed text

--- src/components/welcomecard/welcomecard.tsx
+++ src/components/welcomecard/welcomecard.tsx
@@ -8,15 +8,17 @@ const WelcomeCard = () => {
     <div id={"welcome"} className="h-full grid grid-rows-2 gap-0 xl:gap-30">
       <div className="p-5 sm:p-0 flex flex-col items-center justify-center bg-green-0 gap-10">
         <div className="text-3xl lg:text-5xl text-center text-wrap">
-          אני עוזר לכם לממש את הפוטנציאל שלכם
+          שחר דהן – מאמן כושר וטריאתלון מוסמך
         </div>
         <div
           dir="rtl"
           className="text-(--sub-header) text-wrap sm:w-md text-center"
         >
-          בעל סטודיו בוטיק לאימוני כושר. <br />
-          מאמן קבוצות רכיבת שטח למסטרס, ובונה תוכניות אימון אישיות ליעדים שלכם.
-          מתמחה בהכנת מתאמנים לתחרויות טריאתלון ואיש הברזל, כולל ליווי תזונתי.
+          בעל סטודיו בוטיק לאימוני כושר מגוונים: אימוני כוח, חיטוב ועיצוב,
+          אימונים פונקציונליים ופילאטיס מזרן. מאמן קבוצות רכיבת שטח למסטרס,
+          ומתאים תוכניות אימון אישיות ליעדים שלכם. מתמחה בהכנת מתאמנים לתחרויות
+          טריאתלון ואיש הברזל, כולל ליווי תזונתי. <br />
+          החזון שלי: לעזור לכם לממש את הפוטנציאל – וליהנות מהדרך!
         </div>
         <Dialog>
           <DialogTrigger asChild>

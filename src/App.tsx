ba8c1fa2f314f220d3664252e75f7049a78dd9fb
Sean Dahan <aui.svi@gmail.com>
2025-02-08 14:14:09 +0200

    Changed logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="text-3xl ">
+    <div className="text-3xl">
       <p className="font-[AuroRumpthut] font-bold">Shahar Dahan</p>
       Hey there This site is under construction.
     </div>

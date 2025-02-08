24a85374a87a83c5ecd8d2f2365b7ac7349b9495
Sean Dahan <aui.svi@gmail.com>
2025-02-08 14:08:23 +0200

    Chages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import "@/fonts/Auro Rumpthut.otf";
+import "@/fonts/Auro Rumpthut.ttf";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(<App />);
